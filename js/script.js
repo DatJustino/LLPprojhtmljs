754533280f5e5d06a9a4727fc489dc86f0dfc0ac
DatJustino <100353161+DatJustino@users.noreply.github.com>
2023-05-17 13:08:36 +0200

    refactored names so all lowercase for commission because of bugs, Crud functions work for commission, course and blog.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 const endpoint = "http://localhost:8080/commissions";
 let commissions;
+let currentCommissionId;
 
 window.addEventListener("load", initApp);
 
@@ -10,16 +11,17 @@ function initApp() {
 
     // event listeners
     document.querySelector("#createCommissionButton").addEventListener("click", showCreateCommissionDialog);
-    document.querySelector("#create-commission-form").addEventListener("submit", createCommissionClicked);
+    document.querySelector("#create-commission-form").addEventListener("submit", handleCreateCommission);
     document.querySelector("#update-commission-form").addEventListener("submit", updateCommissionClicked);
     document.querySelector("#delete-commission-form").addEventListener("submit", deleteCommissionClicked);
     document.querySelector("#delete-commission-dialog .btn-cancel").addEventListener("click", deleteCancelClicked);
-/*
-    document.querySelector("#select-sort-by").addEventListener("change", sortByChanged);
-*/
-/*    document.querySelector("#input-search").addEventListener("keyup", inputSearchChanged);
-    document.querySelector("#input-search").addEventListener("search", inputSearchChanged);*/
+    /*
+        document.querySelector("#select-sort-by").addEventListener("change", sortByChanged);
+    */
+    /*    document.querySelector("#input-search").addEventListener("keyup", inputSearchChanged);
+        document.querySelector("#input-search").addEventListener("search", inputSearchChanged);*/
 }
+
 
 // ============== Events ============== //
 
@@ -27,53 +29,40 @@ function showCreateCommissionDialog() {
     document.querySelector("#create-commission-dialog").style.display = "block"; // show create commission dialog
 }
 
-function createCommissionClicked(event) {
-    event.preventDefault();
-    const form = event.target;
-    const fName = form.elements.comFName.value;
-    const lName = form.elements.comLName.value;
-    const email = form.elements.comEmail.value;
-    const phoneNumber = form.elements.comPhoneNumber.value;
-    const subject = form.elements.comSubject.value;
-    const description = form.elements.comDescription.value;
-    const pageFormat1 = form.elements.comPageFormat1.value;
-    const pageFormat2 = form.elements.comPageFormat2.value;
-    const deliveryDate = form.elements.comDeliveryDate.value;
-    const street = form.elements.comStreet.value;
-    const houseNumber = form.elements.comHouseNumber.value;
-    const floor = form.elements.comFloor.value;
-    const zipCode = form.elements.comZipCode.value;
-    const image1 = form.elements.comImage1.files[0];
-    const image2 = form.elements.comImage2.files[0];
-    const image3 = form.elements.comImage3.files[0];
 
-    createCommission(fName, lName, email, phoneNumber, subject,
-        description, pageFormat1, pageFormat2, deliveryDate, street,
-        houseNumber, floor, zipCode, image1, image2, image3);
-    form.reset();
-}
-async function createCommission(fName, lName, email, phoneNumber, subject, description, pageFormat1, pageFormat2, deliveryDate, street, houseNumber, floor, zipCode, image1, image2, image3) {
+function handleCreateCommission(event) {
+    showCreateCommissionDialog();
+    event.preventDefault();
+    /*    const form = event.target;*/
+    const form = event.target;
     const commissionData = {
-        firstName: fName,
-        lastName: lName,
-        email: email,
-        phoneNumber: phoneNumber,
-        subject: subject,
-        description: description,
-        pageFormat1: pageFormat1,
-        pageFormat2: pageFormat2,
-        deliveryDate: deliveryDate,
-        street: street,
-        houseNumber: houseNumber,
-        floor: floor,
-        zipCode: zipCode,
+        firstname: form.elements.firstname.value,
+        lastname: form.elements.lastname.value,
+        email: form.elements.email.value,
+        phonenumber: form.elements.phonenumber.value,
+        subject: form.elements.subject.value,
+        description: form.elements.description.value,
+        pageformat1: form.elements.pageformat1.value,
+        pageformat2: form.elements.pageformat2.value,
+        deliverydate: form.elements.deliverydate.value,
+        street: form.elements.street.value,
+        housenumber: form.elements.housenumber.value,
+        floor: form.elements.floor.value,
+        zipcode: form.elements.zipcode.value,
+        imageurl1: form.elements.imageurl1.value,
+        imageurl2: form.elements.imageurl2.value,
+        imageurl3: form.elements.imageurl3.value
     };
 
-    const formData = new FormData();
-    formData.append("image1", image1);
-    formData.append("image2", image2);
-    formData.append("image3", image3);
-//TODO: Datatype of pictures are wrong error 415 (Unsupported Media Type) !!!!! FIX THIS
+    createCommission(commissionData);
+    console.log(form)
+    form.reset();
+    hideCreateCommissionModal();
+}
+
+async function createCommission(commissionData) {
+    const formData = createFormData(commissionData);
+
     const response = await fetch(`${endpoint}/create`, {
         method: "POST",
         body: formData,
@@ -85,30 +74,12 @@ async function createCommission(fName, lName, email, phoneNumber, subject, descr
     }
 }
 
-function deleteCommissionClicked(event) {
-    event.preventDefault();
-    const id = event.target.getAttribute("data-id");
-    deleteCommission(id);
-}
-
-function deleteCancelClicked() {
-    document.querySelector("#delete-commission-dialog").style.display = "none";
-}
-
-function sortByChanged(event) {
-    const selectedValue = event.target.value;
-
-    if (selectedValue === "firstName") {
-        commissions.sort(compareFirstName);
-    } else if (selectedValue === "lastName") {
-        commissions.sort(compareLastName);
+function createFormData(commissionData) {
+    const formData = new FormData();
+    for (const key in commissionData) {
+        formData.append(key, commissionData[key]);
     }
-
-    showCommissions(commissions);
-}
-async function updateCommissionsTable() {
-    commissions = await getCommissions();
-    showCommissions(commissions);
+    return formData;
 }
 
 /*
@@ -118,6 +89,19 @@ function inputSearchChanged(event) {
     showCommissions(commissionsToShow);
 }
 */
+
+/*function sortByChanged(event) {
+    const selectedValue = event.target.value;
+
+    if (selectedValue === "firstName") {
+        commissions.sort(compareFirstName);
+    } else if (selectedValue === "lastName") {
+        commissions.sort(compareLastName);
+    }
+
+    showCommissions(commissions);
+}*/
+
 
 // ============== Commissions ============== //
 
@@ -129,6 +113,11 @@ async function getCommissions() {
     return commissions;
 }
 
+async function updateCommissionsTable() {
+    commissions = await getCommissions();
+    showCommissions(commissions);
+}
+
 function showCommissions(listOfCommissions) {
     const tableBody = document.querySelector("#commissionsTable tbody");
     tableBody.innerHTML = "";
@@ -136,6 +125,7 @@ function showCommissions(listOfCommissions) {
     for (const commission of listOfCommissions) {
         showCommission(commission);
     }
+
 }
 
 function showCommission(commissionObject) {
@@ -144,19 +134,22 @@ function showCommission(commissionObject) {
     const row = document.createElement("tr");
     row.innerHTML = `
   <td>${commissionObject.commissionId}</td>
-  <td>${commissionObject.fname}</td>
-  <td>${commissionObject.lname}</td>
+  <td>${commissionObject.firstname}</td>
+  <td>${commissionObject.lastname}</td>
   <td>${commissionObject.email}</td>
-  <td>${commissionObject.phoneNumber}</td>
+  <td>${commissionObject.phonenumber}</td>
   <td>${commissionObject.subject}</td>
   <td>${commissionObject.description}</td>
-  <td>${commissionObject.pageFormat1}</td>
-  <td>${commissionObject.pageFormat2}</td>
-  <td>${commissionObject.deliveryDate}</td>
+  <td>${commissionObject.pageformat1}</td>
+  <td>${commissionObject.pageformat2}</td>
+  <td>${commissionObject.deliverydate}</td>
   <td>${commissionObject.street}</td>
-  <td>${commissionObject.houseNumber}</td>
+  <td>${commissionObject.housenumber}</td>
   <td>${commissionObject.floor}</td>
-  <td>${commissionObject.zipCode}</td>
+  <td>${commissionObject.zipcode}</td>
+  <td>${commissionObject.imageurl1}</td>
+  <td>${commissionObject.imageurl2}</td>
+  <td>${commissionObject.imageurl3}</td>
   <td>
       <button class="editCommissionButton btn btn-com" data-id="${commissionObject.commissionId}">Edit</button>
       <button class="deleteCommissionButton btn btn-delete" data-id="${commissionObject.commissionId}">Delete</button>
@@ -165,9 +158,41 @@ function showCommission(commissionObject) {
     tableBody.appendChild(row);
 
     // add event listeners to com and delete buttons
-    row.querySelector(".editCommissionButton").addEventListener("click", updateCommissionClicked);
-    row.querySelector(".deleteCommissionButton").addEventListener("click", deleteCommissionClicked);
+    row.querySelector(".editCommissionButton").addEventListener("click", function () {
+        currentCommissionId = commissionObject.commissionId;
+        showUpdateCommissionDialog(commissionObject);
+    });
+    row.querySelector(".deleteCommissionButton").addEventListener("click", function () {
+        currentCommissionId = commissionObject.commissionId;
+        showDeleteCommissionDialog(commissionObject);
+    });
 }
+
+function showUpdateCommissionDialog(commission) {
+    // Show the update dialog
+    document.querySelector("#update-commission-dialog").style.display = "block";
+
+    // Fill in the form with the existing commission values
+    const form = document.querySelector("#update-commission-form");
+    form.querySelector("#firstname").value = commission.firstname;
+    form.querySelector("#lastname").value = commission.lastname;
+    form.querySelector("#email").value = commission.email;
+    form.querySelector("#phonenumber").value = commission.phonenumber;
+    form.querySelector("#subject").value = commission.subject;
+    form.querySelector("#description").value = commission.description;
+    form.querySelector("#pageformat1").value = commission.pageformat1;
+    form.querySelector("#pageformat2").value = commission.pageformat2;
+    form.querySelector("#deliverydate").value = commission.deliverydate;
+    form.querySelector("#street").value = commission.street;
+    form.querySelector("#housenumber").value = commission.housenumber;
+    form.querySelector("#floor").value = commission.floor;
+    form.querySelector("#zipcode").value = commission.zipcode;
+    form.querySelector("#imageurl1").value = commission.imageurl1;
+    form.querySelector("#imageurl2").value = commission.imageurl2;
+    form.querySelector("#imageurl3").value = commission.imageurl3;
+}
+
+/*
 function searchCommissions(searchValue) {
     searchValue = searchValue.toLowerCase();
 
@@ -178,8 +203,94 @@ function searchCommissions(searchValue) {
 
     return results;
 }
+*/
 
 
+async function updateCommission(id, firstname, lastname, email, phonenumber, subject, description,
+                                pageformat1, pageformat2, deliverydate, street, housenumber,
+                                floor, zipcode, imageurl1, imageurl2, imageurl3) {
+    const commissionData = {
+        commissionId: id,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        phonenumber: phonenumber,
+        subject: subject,
+        description: description,
+        pageformat1: pageformat1,
+        pageformat2: pageformat2,
+        deliverydate: deliverydate,
+        street: street,
+        housenumber: housenumber,
+        floor: floor,
+        zipcode: zipcode,
+        imageurl1: imageurl1,
+        imageurl2: imageurl2,
+        imageurl3: imageurl3,
+    };
+
+    const formData = new FormData();
+    formData.append("firstname", firstname);
+    formData.append("lastname", lastname);
+    formData.append("email", email);
+    formData.append("phonenumber", phonenumber);
+    formData.append("subject", subject);
+    formData.append("description", description);
+    formData.append("pageformat1", pageformat1);
+    formData.append("pageformat2", pageformat2);
+    formData.append("deliverydate", deliverydate);
+    formData.append("street", street);
+    formData.append("housenumber", housenumber);
+    formData.append("floor", floor);
+    formData.append("zipcode", zipcode);
+    formData.append("imageurl1", imageurl1);
+    formData.append("imageurl2", imageurl2);
+    formData.append("imageurl3", imageurl3);
+    console.log(formData);
+    const response = await fetch(`${endpoint}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(commissionData),
+    });
+    if (response.ok) {
+        console.log("Commission successfully updated");
+        updateCommissionsTable();
+    }
+}
+
+function updateCommissionClicked(event) {
+    event.preventDefault();
+    const form = event.target;
+    const id = event.target.getAttribute("data-id");
+    const commission = commissions.find(commission => commission.commissionId == id);
+
+    const firstname = form.querySelector("#firstname").value;
+    const lastname = form.querySelector("#lastname").value;
+    const email = form.querySelector("#email").value;
+    const phonenumber = form.querySelector("#phonenumber").value;
+    const subject = form.querySelector("#subject").value;
+    const description = form.querySelector("#description").value;
+    const pageformat1 = form.querySelector("#pageformat1").value;
+    const pageformat2 = form.querySelector("#pageformat2").value;
+    const deliverydate = form.querySelector("#deliverydate").value;
+    const street = form.querySelector("#street").value;
+    const housenumber = form.querySelector("#housenumber").value;
+    const floor = form.querySelector("#floor").value;
+    const zipcode = form.querySelector("#zipcode").value;
+    const imageurl1 = form.querySelector("#imageurl1").value;
+    const imageurl2 = form.querySelector("#imageurl2").value;
+    const imageurl3 = form.querySelector("#imageurl3").value;
+    console.log(id, firstname, lastname, email, phonenumber, subject, description,
+        pageformat1, pageformat2, deliverydate, street, housenumber,
+        floor, zipcode, imageurl1, imageurl2, imageurl3);
+    updateCommission(currentCommissionId, firstname, lastname, email, phonenumber, subject,
+        description, pageformat1, pageformat2, deliverydate, street,
+        housenumber, floor, zipcode, imageurl1, imageurl2, imageurl3);
+
+    form.reset();
+}
 
 async function deleteCommission(id) {
     const response = await fetch(`${endpoint}/${id}`, {
@@ -192,71 +303,25 @@ async function deleteCommission(id) {
     }
 }
 
-async function updateCommission(id, fName, lName, email, phoneNumber, subject, description,
-                                pageFormat1, pageFormat2, deliveryDate, street, houseNumber,
-                                floor, zipCode, image1, image2, image3) {
-    const commissionData = {
-        commissionId: id,
-        firstName: fName,
-        lastName: lName,
-        email: email,
-        phoneNumber: phoneNumber,
-        subject: subject,
-        description: description,
-        pageFormat1: pageFormat1,
-        pageFormat2: pageFormat2,
-        deliveryDate: deliveryDate,
-        street: street,
-        houseNumber: houseNumber,
-        floor: floor,
-        zipCode: zipCode,
-    };
-
-    const formData = new FormData();
-    formData.append("image1", image1);
-    formData.append("image2", image2);
-    formData.append("image3", image3);
-
-    const response = await fetch(`${endpoint}/${id}`, {
-        method: "PUT",
-        body: formData,
-    });
-
-    if (response.ok) {
-        console.log("Commission successfully updated");
-        updateCommissionsTable();
-    }
-}
-function updateCommissionClicked(event) {
+function deleteCommissionClicked(event) {
     event.preventDefault();
-    const form = event.target;
-    const id = form.elements.commissionId.value;
-    const fName = form.elements.comFName.value;
-    const lName = form.elements.comLName.value;
-    const email = form.elements.comEmail.value;
-    const phoneNumber = form.elements.comPhoneNumber.value;
-    const subject = form.elements.comSubject.value;
-    const description = form.elements.comDescription.value;
-    const pageFormat1 = form.elements.comPageFormat1.value;
-    const pageFormat2 = form.elements.comPageFormat2.value;
-    const deliveryDate = form.elements.comDeliveryDate.value;
-    const street = form.elements.comStreet.value;
-    const houseNumber = form.elements.comHouseNumber.value;
-    const floor = form.elements.comFloor.value;
-    const zipCode = form.elements.comZipCode.value;
-    const image1 = form.elements.comImage1.files[0];
-    const image2 = form.elements.comImage2.files[0];
-    const image3 = form.elements.comImage3.files[0];
-    updateCommission(id, fName, lName, email, phoneNumber, subject,
-        description, pageFormat1, pageFormat2, deliveryDate, street,
-        houseNumber, floor, zipCode, image1, image2, image3);
-    form.reset();
+    const id = event.target.getAttribute("data-id");
+    deleteCommission(id);
+    document.querySelector("#delete-commission-dialog").style.display = "none";
+
 }
+
+function deleteCancelClicked() {
+    document.querySelector("#delete-commission-dialog").style.display = "none";
+}
+
 
 // ============== Helper Functions ============== //
 function prepareData(dataObject) {
     return dataObject;
 }
+
+
 /*function prepareData(dataObject) {
     const commissionsArray = [];
 
@@ -268,6 +333,36 @@ function prepareData(dataObject) {
 
     return commissionsArray;
 }*/
+
+function hideCreateCommissionModal() {
+    const modal = document.querySelector("#create-commission-dialog");
+    modal.style.display = 'none';
+}
+
+function hideEditCommissionModal() {
+    const modal = document.querySelector("#edit-commission-dialog");
+    modal.style.display = 'none';
+}
+
+
+function showDeleteCommissionDialog(commissionObject) {
+    // Prevent form submission
+
+    // Retrieve the ID of the commission to be deleted
+    const id = commissionObject.commissionId;
+
+    // Find the corresponding commission data
+    const commission = commissions.find(commission => commission.commissionId == id);
+
+    // Set the title of the dialog to the commission's name for confirmation
+    document.querySelector("#dialog-delete-post-title").textContent = `${commission.firstname} ${commission.lastname}`;
+
+    // Store the ID in the form for deletion
+    document.querySelector("#delete-commission-form").setAttribute("data-id", id);
+
+    // Show the delete commission dialog
+    document.querySelector("#delete-commission-dialog").style.display = "block";
+}
 
 function compareFirstName(a, b) {
     return a.firstName.localeCompare(b.firstName);
